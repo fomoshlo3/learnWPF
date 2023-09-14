3eb85e034d0242ac912ad071072535fcabfa4ab5
Hannes Kasel <hannes.kasel@advokat.at>
2023-09-14 15:30:33 +0200

    documentation

---
+++ Microsoft.DataBindingSampleWpf/Models/ColorDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.DataBindingSampleWpf.Resources;
+using System.Windows.Media;
+
+namespace Microsoft.DataBindingSampleWpf.Models
+{
+    public class ColorDescriptor : ObservableObject
+    {
+        public Color Color { get; private set; }
+
+        public string Name { get; private set; }
+
+        public ColorDescriptor(Color color, string name)
+        {
+            Color = color;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"#{Name} (#{Color.R:X2}{Color.G:X2}{Color.B:X2})";
+        }
+    }
+}
---
+++ Microsoft.DataBindingSampleWpf/Resources/ColorListDataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.DataBindingSampleWpf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Microsoft.DataBindingSampleWpf.Resources
+{
+    public class ColorListDataContext : ObservableObject
+    {
+        public List<ColorDescriptor> LotsOfColors { get; private set; } //...the naming -.-
+
+        public ColorListDataContext()
+        {
+            LotsOfColors = new List<ColorDescriptor>
+            {
+                new ColorDescriptor(Colors.Red, "red"),
+                new ColorDescriptor(Colors.White, "white"),
+                new ColorDescriptor(Colors.Green, "green"),
+                new ColorDescriptor(Colors.Yellow, "yellow"),
+                new ColorDescriptor(Colors.Blue, "blue"),
+                new ColorDescriptor(Colors.Black, "black")
+            };
+
+        }
+    }
+}
